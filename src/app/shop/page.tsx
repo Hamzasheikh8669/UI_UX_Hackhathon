6970ebb5e0258e484730da9cbcc28542c8be80b1
Hamza sheikh866 <Hamzasheikh8666@gmail.com>
2024-12-31 22:08:54 +0500

    fixed error

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -6,152 +6,149 @@ import Features from "@/components/features";
 export default function ShopHero() {
   const products = [
     {
-      Id: 1,
+      id: 1, // Changed 'Id' to 'id' to follow standard naming convention
       name: "Syltherine",
       description: "Stylish cafe chair",
       price: 2500000,
       image: "/images/img1.png",
-      tag: "-30%" as const,
+      tag: "-30%",
     },
     {
-      Id: 2,
+      id: 2,
       name: "Leviosa",
       description: "Stylish cafe chair",
       price: 2500000,
       image: "/images/img2.png",
     },
     {
-      Id: 3,
+      id: 3,
       name: "Lolito",
       description: "Luxury big sofa",
       price: 7000000,
       image: "/images/img3.png",
-      tag: "-30%" as const,
+      tag: "-30%",
     },
     {
-      Id: 4,
+      id: 4,
       name: "Respira",
       description: "Outdoor bar table and stool",
       price: 500000,
       image: "/images/img4.jpeg",
-      tag: "New" as const,
+      tag: "New",
     },
     {
-      Id: 5,
+      id: 5,
       name: "Syltherine",
       description: "Stylish cafe chair",
       price: 2500000,
       image: "/images/img1.png",
-      tag: "-30%" as const,
+      tag: "-30%",
     },
     {
-      Id: 6,
+      id: 6,
       name: "Leviosa",
       description: "Stylish cafe chair",
       price: 2500000,
       image: "/images/img2.png",
     },
     {
-      Id: 7,
+      id: 7,
       name: "Lolito",
       description: "Luxury big sofa",
       price: 7000000,
       image: "/images/img3.png",
-      tag: "-30%" as const,
+      tag: "-30%",
     },
     {
-      Id: 8,
+      id: 8,
       name: "Respira",
       description: "Outdoor bar table and stool",
       price: 500000,
       image: "/images/img4.jpeg",
-      tag: "New" as const,
+      tag: "New",
     },
     {
-      Id: 9,
+      id: 9,
       name: "Syltherine",
       description: "Stylish cafe chair",
       price: 2500000,
       image: "/images/img1.png",
-      tag: "-30%" as const,
+      tag: "-30%",
     },
     {
-      Id: 10,
+      id: 10,
       name: "Leviosa",
       description: "Stylish cafe chair",
       price: 2500000,
       image: "/images/img2.png",
     },
     {
-      Id: 11,
+      id: 11,
       name: "Lolito",
       description: "Luxury big sofa",
       price: 7000000,
       image: "/images/img3.png",
-      tag: "-30%" as const,
+      tag: "-30%",
     },
     {
-      Id: 12,
+      id: 12,
       name: "Respira",
       description: "Outdoor bar table and stool",
       price: 500000,
       image: "/images/img4.jpeg",
-      tag: "New" as const,
+      tag: "New",
     },
     {
-      Id: 13,
+      id: 13,
       name: "Syltherine",
       description: "Stylish cafe chair",
       price: 2500000,
       image: "/images/img1.png",
-      tag: "-30%" as const,
+      tag: "-30%",
     },
     {
-      Id: 14,
+      id: 14,
       name: "Leviosa",
       description: "Stylish cafe chair",
       price: 2500000,
       image: "/images/img2.png",
     },
     {
-      Id: 15,
+      id: 15,
       name: "Lolito",
       description: "Luxury big sofa",
       price: 7000000,
       image: "/images/img3.png",
-      tag: "-30%" as const,
+      tag: "-30%",
     },
     {
-      Id: 16,
+      id: 16,
       name: "Respira",
       description: "Outdoor bar table and stool",
       price: 500000,
       image: "/images/img4.jpeg",
-      tag: "New" as const,
+      tag: "New",
     },
-
-  
   ];
 
   return (
     <>
-    <ShopHeader />
+      <ShopHeader />
       <section className="py-12 lg:py-16">
         <div className="container mx-auto px-4 lg:px-20 text-center">
           {/* Responsive Grid for Products */}
           <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {products.map((product) => (
-              <ProductCard key={product.name} {...product} />
+              <ProductCard key={product.id} {...product} />
             ))}
           </div>
         </div>
       </section>
       <div className="space-y-12">
-      {/* Other shop content */}
-      <Pagination currentPage={1} totalPages={3} />
-      <Features />
+        {/* Other shop content */}
+        <Pagination currentPage={1} totalPages={3} />
+        <Features />
       </div>
-    
     </>
   );
 }
